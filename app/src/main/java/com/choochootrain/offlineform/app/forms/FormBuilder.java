ff2a0f3aabdb233cec5fe72f0296f0d4bc3c3054
Hurshal Patel <hpatel516@gmail.com>
2014-03-31 23:47:58 -0700

    add basic gson parsing

--- app/src/main/java/com/choochootrain/offlineform/app/forms/FormBuilder.java
+++ app/src/main/java/com/choochootrain/offlineform/app/forms/FormBuilder.java
@@ -3,28 +3,51 @@ package com.choochootrain.offlineform.app.forms;
 import android.content.Context;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
 
 public class FormBuilder {
     private static final String TAG = "FormBuilder";
     private Context context;
+    private Gson gson;
 
     public FormBuilder(Context context) {
         this.context = context;
+        this.gson = new Gson();
     }
 
     public void populateForm(LinearLayout layout, String formDataString) {
-        JSONObject formData;
-        try {
-            formData = new JSONObject(formDataString);
-        } catch (JSONException e) {
-           Log.e(TAG, "error parsing json");
-           return;
-        }
+        FormConfig formConfig = gson.fromJson(formDataString, FormConfig.class);
 
-        Toast.makeText(context, formData.optString("title", "asdf"), Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, formConfig.title, Toast.LENGTH_SHORT).show();
+        for (int i = 0; i < formConfig.elements.length; i++) {
+            addFormElement(layout, formConfig.elements[i]);
+        }
+    }
+
+    private void addFormElement(LinearLayout layout, FormElement element) {
+        //TODO implement other types
+        if (element.type.equals("text")) {
+            TextView t = new TextView(context);
+            t.setText("text " + element.name);
+            layout.addView(t);
+        } else if (element.type.equals("numeric")) {
+            TextView t = new TextView(context);
+            t.setText("numeric " + element.name);
+            layout.addView(t);
+        }
+    }
+
+    private class FormConfig {
+        private String title;
+        private FormElement[] elements;
+    }
+
+    private class FormElement {
+        private String name;
+        private String type;
+        private String id;
     }
 }
