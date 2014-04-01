46f6c338e6323a229ee9da1d5e6aac93e61e11f9
Hurshal Patel <hpatel516@gmail.com>
2014-04-01 00:51:15 -0700

    add form submit button

--- app/src/main/java/com/choochootrain/offlineform/app/forms/FormBuilder.java
+++ app/src/main/java/com/choochootrain/offlineform/app/forms/FormBuilder.java
@@ -2,6 +2,8 @@ package com.choochootrain.offlineform.app.forms;
 
 import android.content.Context;
 import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -18,13 +20,26 @@ public class FormBuilder {
         this.gson = new Gson();
     }
 
-    public void populateForm(LinearLayout layout, String formDataString) {
-        FormConfig formConfig = gson.fromJson(formDataString, FormConfig.class);
+    public void populateForm(final LinearLayout layout, String formDataString) {
+        final FormConfig formConfig = gson.fromJson(formDataString, FormConfig.class);
 
         Toast.makeText(context, formConfig.title, Toast.LENGTH_SHORT).show();
         for (int i = 0; i < formConfig.elements.length; i++) {
             addFormElement(layout, formConfig.elements[i]);
         }
+
+        Button submitButton = new Button(context);
+        submitButton.setText("submit");
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = ((EditText)layout.findViewById(formConfig.elements[0].id.hashCode())).getText().toString()
+                + ((EditText)layout.findViewById(formConfig.elements[1].id.hashCode())).getText().toString()
+                + ((EditText)layout.findViewById(formConfig.elements[2].id.hashCode())).getText().toString();
+                Toast.makeText(context, data, Toast.LENGTH_SHORT).show();
+            }
+        });
+        layout.addView(submitButton);
     }
 
     private void addFormElement(LinearLayout layout, FormElement element) {
@@ -33,11 +48,13 @@ public class FormBuilder {
             EditText t = new EditText(context);
             t.setInputType(InputType.TYPE_CLASS_TEXT);
             t.setText(element.name);
+            t.setId(element.id.hashCode());
             layout.addView(t);
         } else if (element.type.equals("numeric")) {
             EditText t = new EditText(context);
             t.setInputType(InputType.TYPE_CLASS_NUMBER);
             t.setText(element.name);
+            t.setId(element.id.hashCode());
             layout.addView(t);
         }
     }
